ce060f35680a0cd9505d9be6045adf96eaf89405
Ruan Pablo <ruanfaluke@gmail.com>
2023-04-29 13:20:36 -0300

    Customers Component new transition

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -17,42 +17,42 @@ export function Customers() {
                     <p className="font-semibold text-medium text-lg mt-6 text-center">Conheça a satisfação dos nossos clientes e saiba por que <br /> somos a escolha número um!</p>
                 </div>
                 <div className='flex flex-wrap mt-12 mb-12 gap-x-4 gap-y-4 items-center justify-center'>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-left" data-aos-duration="2000" className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://servers-live.fivem.net/servers/icon/x43r9m/376926146.png"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>NEXUS ROLEPLAY</p>
                             <small className='font-medium'>138.500 membros</small>
                         </div>
                     </div>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-left" data-aos-duration="2000" className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://images-ext-1.discordapp.net/external/Y8JHvgcOXQeFCONpzC5fqhYXmtrr3jUf-cEZb7bq2Rk/https/i.imgur.com/7OpJDlr.gif"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>RJ ACADEMY</p>
                             <small className='font-medium'>58.002 membros</small>
                         </div>
                     </div>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-up" data-aos-duration="2000" className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://images-ext-2.discordapp.net/external/HdsKUigxZuGDJ6T_w6K1ylI4DzaGtLXbKLax9YQSIfA/https/imgur.com/QLzgRoR.gif"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>RACE ULTIMATE</p>
                             <small className='font-medium'>138.500 membros</small>
                         </div>
                     </div>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-left" data-aos-duration="2000" className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://servers-live.fivem.net/servers/icon/wyq9ey/-1109026505.png"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>INFINITY ROLEPLAY</p>
                             <small className='font-medium'>138.500 membros</small>
                         </div>
                     </div>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-left" data-aos-duration="2000" className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://images-ext-2.discordapp.net/external/KJsDxIG1xj3jT31Q72UsqjydS7J7ng4HmhfFBWlFuG4/https/imgur.com/GE5nJxj.gif"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>FIRE EVOLUTION</p>
                             <small className='font-medium'>43.500 membros</small>
                         </div>
                     </div>
-                    <div className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
+                    <div data-aos="flip-left" data-aos-duration="2000"  className='w-480 h-20 bg-gray-customer flex gap-x-4 px-6 items-center rounded-lg'>
                         <img className='w-12 h-12 rounded-full' src="https://servers-live.fivem.net/servers/icon/wxd9jg/1898114881.png"/>
                         <div className='flex flex-col text-left'>
                             <p className='font-bold'>KUSH NETWORK</p>
