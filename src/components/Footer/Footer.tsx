3faf656faab774f711f90c03ba1e719885d6b513
Ruan Pablo <ruanfaluke@gmail.com>
2023-04-29 20:14:35 -0300

    sadfmsdfjk

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ export function Footer() {
     return(
         <>
         <div className="w-screen h-52 bg-gray-customer-background flex items-center justify-around border-2 border-solid border-black-border-footer">
-            <img className="opacity-10 grayscale" src={logo}/>
-            <small className="grayscale text-gray-500">© Copyright Denver Studio. Todos direitos reservados. <br />  Nosso email para contato: denverstudio.br@gmail.com</small>
+            <img className="opacity-10 grayscale max-2xl:w-16" src={logo}/>
+            <small className="grayscale text-gray-500 max-2xl:text-[9px]">© Copyright Denver Studio. Todos direitos reservados. <br />  Nosso email para contato: denverstudio.br@gmail.com</small>
         </div>
         </>
     )
