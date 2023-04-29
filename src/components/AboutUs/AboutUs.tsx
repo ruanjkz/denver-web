4ab12f5eacd1f39732b4e43f29991ee00641ddb1
Ruan Pablo <ruanfaluke@gmail.com>
2023-04-29 20:29:30 -0300

    sdgfsf

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -19,17 +19,17 @@ export function AboutUs () {
                     <div data-aos="zoom-in" data-aos-duration="2000" className="w-480 h-465 bg-gray-box border-2 border-solid border-gray-box-border rounded-xl flex flex-col py-8 px-10 items-center gap-y-6 max-2xl:w-[330px]">
                         <img draggable="false" className="w-20" src={protect}/>
                         <p className="text-2xl font-medium">Proteção</p>
-                        <small className="text-left text-base max-[384px]:w-[270px]">A proteção por autenticação é uma medida de segurança importante para garantir que somente usuários autorizados possam acessar e executar determinado script em um ambiente como o FiveM. Essa medida de segurança ajuda a evitar o vazamento de recursos e scripts do servidor, o que pode comprometer a segurança do sistema e dos usuários.</small>
+                        <small className="text-left text-base max-[384px]:w-[270px] max-[414px]:text-[14px]">A proteção por autenticação é uma medida de segurança importante para garantir que somente usuários autorizados possam acessar e executar determinado script em um ambiente como o FiveM. Essa medida de segurança ajuda a evitar o vazamento de recursos e scripts do servidor, o que pode comprometer a segurança do sistema e dos usuários.</small>
                     </div>
                     <div data-aos="zoom-in"  data-aos-duration="2000" className="w-480 h-465 bg-gray-box border-2 border-solid border-gray-box-border rounded-xl flex flex-col py-8 px-10 items-center gap-y-6 max-2xl:w-[330px]">
                         <img draggable="false" className="w-20" src={support}/>
                         <p className="text-2xl font-medium">Suporte</p>
-                        <small className="text-left text-base max-[384px]:w-[270px]">Oferecemos um suporte de alta qualidade para nossos clientes, que está disponível de segunda a sexta-feira, das 9h às 20h, e aos sábados, das 12h às 16h. Nosso suporte é projetado para ajudar nossos clientes a resolver quaisquer problemas ou dúvidas que possam surgir durante o uso de nossos produtos ou serviços.</small>
+                        <small className="text-left text-base max-[384px]:w-[270px] max-[414px]:text-[14px]">Oferecemos um suporte de alta qualidade para nossos clientes, que está disponível de segunda a sexta-feira, das 9h às 20h, e aos sábados, das 12h às 16h. Nosso suporte é projetado para ajudar nossos clientes a resolver quaisquer problemas ou dúvidas que possam surgir durante o uso de nossos produtos ou serviços.</small>
                     </div>
                     <div data-aos="zoom-in"  data-aos-duration="2000" className="w-480 h-465 bg-gray-box border-2 border-solid border-gray-box-border rounded-xl flex flex-col py-8 px-10 items-center gap-y-6 max-2xl:w-[330px]">
                         <img draggable="false" className="w-20" src={optimize}/>
                         <p className="text-2xl font-medium">Otimização</p>
-                        <small className="text-left text-base max-[384px]:w-[270px]">Nosso principal objetivo é garantir a satisfação dos nossos clientes e dos jogadores de seus servidores. Para isso, temos um compromisso com a otimização de nossos recursos, com o objetivo de oferecer a melhor experiência possível em termos de desempenho e eficiência.</small>
+                        <small className="text-left text-base max-[384px]:w-[270px] max-[414px]:text-[14px]">Nosso principal objetivo é garantir a satisfação dos nossos clientes e dos jogadores de seus servidores. Para isso, temos um compromisso com a otimização de nossos recursos, com o objetivo de oferecer a melhor experiência possível em termos de desempenho e eficiência.</small>
                     </div>
                 </div>
         </>
